109e3fb86e01b47c5a219850cef9ce88b9d8cabf
haifaarouri <haifa.arouri@esprit.tn>
2023-10-08 20:12:44 +0200

    CRUD publication backend

---
+++ MSPublication-1/src/main/java/tn/esprit/microservice/MsPublication1Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPublication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPublication1Application.class, args);
+	}
+
+}
