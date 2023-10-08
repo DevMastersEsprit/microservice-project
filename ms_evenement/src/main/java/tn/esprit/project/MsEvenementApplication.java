145c59612edb54028f29b82164fd6263c40ebf0d
WacefEssaies <wacef.essaies@esprit.tn>
2023-10-08 20:16:08 +0100

    added crud evenement

---
+++ ms_evenement/src/main/java/tn/esprit/project/MsEvenementApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEvenementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEvenementApplication.class, args);
+	}
+
+}
