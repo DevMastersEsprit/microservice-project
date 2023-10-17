b1b2ea872e7a200c9f1a693a179cab2ea2ef026a
Ihebbh <iheb.benhelel@esprit.tn>
2023-10-17 20:40:18 +0100

    eureuka server and client MS page

--- backend/Page Microservice/src/main/java/com/example/demo/PageMicroserviceApplication.java
+++ backend/Page Microservice/src/main/java/com/example/demo/PageMicroserviceApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PageMicroserviceApplication {
 
 	public static void main(String[] args) {
