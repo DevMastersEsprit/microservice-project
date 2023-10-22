239fe7f834a24699e4e750e501a8bce8c21a88df
Ihebbh <iheb.benhelel@esprit.tn>
2023-10-22 18:26:33 +0100

    fix project , docker and apigetway

--- ms-page/src/test/java/com/example/demo/MsPageApplicationTests.java
+++ ms-page/src/test/java/com/example/demo/MsPageApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PageMicroserviceApplicationTests {
+class MsPageApplicationTests {
 
 	@Test
 	void contextLoads() {
